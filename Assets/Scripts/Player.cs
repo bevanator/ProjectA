231708e77442a7d450d4caaff91af7bd53848924
Rimon Akhter <rimona.bevan@gmail.com>
2023-01-08 02:52:22 +0600

    update dotween

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+namespace ProjectA
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float m_Speed;
+        private bool _error;
+
+        private void Start()
+        {
+            What();
+        }
+        private void What()
+        {
+            
+        }
+    }
+}
